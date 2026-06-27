a96b4d7db7f1de43044d8c815d0d4b46da0a8a42
Mukul-prime <Mukulvats200@gmail.com>
2026-06-27 15:20:28 +0530

    Striver dsa Sheet Pattern 12 to be continue ...

--- Pattern_logical_creates/src/PARKING_AREA/Parks.java
+++ Pattern_logical_creates/src/PARKING_AREA/Parks.java
@@ -13,7 +13,7 @@ class Solution {
         }
     }
 
-    public void pattern2(int x, int y) {
+    public void pattern2(int x) {
         for (int i = 0; i <= x; i++) {
             for (int j = 0; j < i; j++) {
                 System.out.print("* ");
@@ -55,14 +55,57 @@ class Solution {
     }
 
     public void pattern6(int x) {
-        for(int i = x ; i>=1 ; i--){
-            for(int j = 1 ;j<=i ; j++){
-                System.out.print(j+" ");
+        for (int i = x; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
             }
             System.out.println();
         }
     }
+
     public void pattern7(int x) {
+
+        for (int i = 0; i < x; i++) {
+
+            for (int creator_space = 0; creator_space < x - i - 1; creator_space++) {
+                System.out.print("  ");
+            }
+
+            for (int plant = 0; plant < 2 * i + 1; plant++) {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+    }
+
+    public void pattern8(int x) {
+        for (int i = 0; i < x; i++) {
+
+            for (int space = 0; space < i; space++) {
+                System.out.print("  ");
+            }
+
+            for (int star = 0; star < 2 * (x - i) - 1; star++) {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+    }
+
+
+    public void pattern12(int x) {
+      int s = 1;
+      for(int i = 0 ; i<x;i++){
+          if(i%2==0) s=1;
+          else s=0;
+          for(int j = 0 ; j<=i ; j++){
+              System.out.print(s+" ");
+              s=1-s;
+          }
+          System.out.println();
+      }
 
     }
 }
@@ -74,11 +117,17 @@ public class Parks {
 //        int m = sc.nextInt();
         Solution s = new Solution();
 //        s.pattern1(n, m);
-//        s.pattern2(n, m);
+//        s.pattern2(n);
 //        s.pattern3(n);
 //        s.pattern4(n);
 //        s.pattern5(n);
 //        s.pattern6(n);
+//        s.pattern7(n);
+//        s.pattern8(n);
+//          s.pattern9(n);
+        s.pattern12(n);
+
+
 
     }
 }
