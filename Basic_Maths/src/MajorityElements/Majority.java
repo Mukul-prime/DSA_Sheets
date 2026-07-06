5d0e82f8482fc293603a02cd5e2cf6f6777ff7cb
Mukul-prime <Mukulvats200@gmail.com>
2026-07-06 17:36:46 +0530

    Work on Episode_2

--- Basic_Maths/src/MajorityElements/Majority.java
+++ Basic_Maths/src/MajorityElements/Majority.java
@@ -24,6 +24,7 @@ class Work{
 public class Majority {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         int t = sc.nextInt();
         int[]  arr = new int[t];
         for (int i = 0; i < t; i++) {
