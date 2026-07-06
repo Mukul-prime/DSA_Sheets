84002a5cf14131015d82e77c41ea3f1caf00c1cf
Mukul-prime <Mukulvats200@gmail.com>
2026-07-06 16:55:08 +0530

    Work on Episode_2

---
+++ Basic_Maths/src/MajorityElements/Majority.java
@@ -0,0 +1,36 @@
+package MajorityElements;
+
+import java.util.*;
+
+
+class Work{
+    public int Worker(int[] a , int n){
+        HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
+        for(int i : a){
+            if(!map.isEmpty() && map.containsKey(i) ){
+                map.put(i,map.get(i)+1);
+            }
+            else{
+                map.put(i,1);
+            }
+        }
+
+     return Collections.max(map.entrySet(), Map.Entry.comparingByValue()).getKey();
+
+    }
+}
+
+
+public class Majority {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        int[]  arr = new int[t];
+        for (int i = 0; i < t; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Work work = new Work();
+        System.out.println(work.Worker(arr,t));
+    }
+}
